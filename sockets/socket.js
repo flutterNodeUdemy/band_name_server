9689cbfb3af43980a9d4d1e201c01daeef588618
oscarruiz86 <oscarruiz86@gmail.com>
2022-03-17 14:26:41 -0500

    5

--- sockets/socket.js
+++ sockets/socket.js
@@ -12,6 +12,12 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log('mensaje', payload);
     });
-    io.emit('mensaje', { admin: 'nuevo mensaje' })
+    io.emit('mensaje', { admin: 'nuevo mensaje' });
+
+
+    client.on('emitir-mensaje', (payload) => {
+        // io.emit('nuevo-mensaje', payload); //emite todos
+        client.broadcast.emit('nuevo-mensaje', payload); //emite menos a emisor
+    });
 
 });
